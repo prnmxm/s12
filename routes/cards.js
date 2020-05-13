f7dcf6b049ee530dcaa5d5a6943ff76d3846df58
Maxim <prnmxm@gmail.com>
2020-05-13 16:58:32 +0700

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,6 @@
+const path = require('path')
 const routes = require('express').Router();
-const data = require('../data/card');
+const data = require(path.resolve('data', 'card.json'));
 
 routes.get('/cards', (req, res) => {
   res.status(200).json(data);
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,17 @@
+const path = require('path')
 const routes = require('express').Router();
-const data = require('../data/user');
+const data = require(path.resolve('data', 'user.json'));
 
 routes.get('/users', (req, res) => {
   res.status(200).json(data);
 });
-routes.get('/users/:id', (req, res) => {
+routes.all('/users/:id', (req, res) => {
   const userId = req.params.id;
   const user = data.find((e) => e._id === userId); //eslint-disable-line
   if (user) {
     res.status(200).json(user);
+  }else {
+    res.status(404).json({ message: 'Нет пользователя с таким id' });
   }
-  res.status(404).json({ message: 'Нет пользователя с таким id' });
 });
 module.exports = routes;
